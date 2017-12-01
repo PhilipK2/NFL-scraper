6e5a5ad7e8561abf89b937c6a580e8490c30e0f6
PhilipK2 <p.c.kappaz2@gmail.com>
2017-11-30 23:34:55 -0600

    reworking plus handlebars

--- mongo-news-scraper-master/server.js
+++ mongo-news-scraper-master/server.js
@@ -1,19 +1,19 @@
+// Our Dependencies
 var express = require("express");
 var exphbs = require("express-handlebars");
 var bodyParser = require("body-parser");
 var logger = require("morgan");
 var mongoose = require("mongoose");
 
+// Using es6 js promise
 mongoose.Promise = Promise;
 
 // Initialize Express
 var app = express();
-var PORT = process.env.port || 3000;
+var PORT = process.env.PORT || 8000;
 
-
-// Use morgan logger for logging requests
+// Use morgan and body parser with our app
 app.use(logger("dev"));
-// Use body-parser for handling form submissions
 app.use(bodyParser.urlencoded({ extended: false }));
 
 // allow the handlesbars engine to be in our toolset
@@ -21,24 +21,27 @@ app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
 // Now set handlebars engine
 app.set('view engine', 'handlebars');
 
-// Use express.static to serve the public folder as a static directory
+// Make public a static dir to serve our static files
 app.use(express.static("public"));
 
-// Set mongoose to leverage built in JavaScript ES6 Promises
-// Connect to the Mongo DB
-
-mongoose.connect("mongodb://heroku_z3tlr6hk:lgubt8tgq6jbeq26aitidn8ea8@ds125896.mlab.com:25896/heroku_z3tlr6hk");
+// Mongoose (orm) connects to our mongo db and allows us to have access to the MongoDB commands for easy CRUD 
+mongoose.connect("mongodb://localhost/week12");
 var db = mongoose.connection;
 
+// if any errors than console errors
+db.on("error", function(error) {
+    console.log("Mongoose Error: ", error);
+});
+
+// display a console message when mongoose has a conn to the db
 db.once("open", function() {
     console.log("Mongoose connection successful.");
 });
 
 // Require the routes in our controllers js file
-require("./controllers/controller.js")(app);
+require("./controllers/articlesController.js")(app);
 
-
-// Start the server
+//Listen on PORT 8000 & notify us.
 app.listen(PORT, function() {
-    console.log("App running on port " + PORT + "!");
+    console.log("App running on port 8 THOUSAND!!!!!!!!");
 });
